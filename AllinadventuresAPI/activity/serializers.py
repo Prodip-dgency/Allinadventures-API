56336a8d83f4ed15315ae8bbb91e2751964e069b
Prodip <p.k.sorkar007@gmail.com>
2022-07-14 17:01:30 +0600

    Serializers updated and media files ignored in git

---
+++ AllinadventuresAPI/activity/serializers.py
@@ -0,0 +1,36 @@
+from dataclasses import fields
+from pyexpat import model
+from rest_framework.serializers import ModelSerializer
+
+from .models import (Location, Category, Gallery, Activity, Content)
+
+
+class LocationModelSerializer(ModelSerializer):
+    class Meta:
+        model = Location
+        fields = "__all__"
+
+class CategoryModelSerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class GalleryModelSerializer(ModelSerializer):
+    class Meta:
+        model = Gallery
+        fields = "__all__"
+
+class CategoryModelSerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class ActivityModelSerializer(ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = "__all__"
+
+class ContentModelSerializer(ModelSerializer):
+    class Meta:
+        model = Content
+        fields = "__all__"
