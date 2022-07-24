fa5b47be3fd710541b19a7a0330c9b9dd219bd9f
Prodip <p.k.sorkar007@gmail.com>
2022-07-24 16:07:18 +0600

    allowed host changed

--- AllinadventuresAPI/AllinadventuresAPI/settings.py
+++ AllinadventuresAPI/AllinadventuresAPI/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [allowed_host, '127.0.0.1']
+ALLOWED_HOSTS = [allowed_host, '127.0.0.1', 'localhost:3000']
 
 
 # Application definition
