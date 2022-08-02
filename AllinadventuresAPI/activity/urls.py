573a5ad6b587b38a40fcb3bf8c7d55821d3e239d
Prodip <p.k.sorkar007@gmail.com>
2022-08-02 13:39:10 +0600

    added location details view (pagedata without serializer)

--- AllinadventuresAPI/activity/urls.py
+++ AllinadventuresAPI/activity/urls.py
@@ -15,6 +15,4 @@ router.register('review', views.ReviewModelView, basename='review')
 
 urlpatterns = [
     path('viewset/', include(router.urls)),
-    path('homepageview/', views.homepageview, name='homepageview'),
-    path('api/all-locations/', views.allLocationView, name='all-locations'),
 ]
